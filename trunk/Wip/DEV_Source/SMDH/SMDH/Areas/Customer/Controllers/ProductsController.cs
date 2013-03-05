53c36eb9839a56e1bae25245a7122b6100f544bc
dovuongnguyen@gmail.com <dovuongnguyen@gmail.com@45282158-0345-991c-288d-0be67aa3d1db>
2013-03-05 03:10:39 +0000

    modify ProductsController

--- trunk/Wip/DEV_Source/SMDH/SMDH/Areas/Customer/Controllers/ProductsController.cs
+++ trunk/Wip/DEV_Source/SMDH/SMDH/Areas/Customer/Controllers/ProductsController.cs
@@ -24,7 +24,7 @@ namespace SMDH.Areas.Customer.Controllers
         public ViewResult Index()
         {
             var product = new List<Product>();
-            product = _repository.Products.Where(p=> p.IsPermanent == true).ToList();
+            product = _repository.Products.Where(p => p.IsPermanent == true).ToList();
             return View(product);
         }
 
@@ -47,6 +47,24 @@ namespace SMDH.Areas.Customer.Controllers
         }
 
         [HttpPost]
+        public ActionResult ConfirmEdit(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repository.Edit(product);
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
         public ActionResult Remove(int id)
         {
             try
@@ -58,6 +76,24 @@ namespace SMDH.Areas.Customer.Controllers
             {
                 return Json(new { success = false });
             }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id)
+        {
+            //var userInfo = context.UserInfoes.Find((Guid)(Membership.GetUser(User.Identity.Name)).ProviderUserKey);
+            //var item = context.ItemManagements.Find(id);
+            //if (item.VendorID != userInfo.CustomerId) throw new HttpException(404, "Not found!");
+            //ViewBag.Customer = userInfo.Customer.CompanyName;
+            //return View(item);
+
+            Product product = _repository.Find(id);
+            return View(product);
         }
 
     }
