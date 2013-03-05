abd947d853798999dc5103bf65e1a4837baf317e
dovuongnguyen@gmail.com <dovuongnguyen@gmail.com@45282158-0345-991c-288d-0be67aa3d1db>
2013-03-05 03:11:47 +0000

    add model product

---
+++ trunk/Wip/DEV_Source/SMDH/SMDH/Models/Abstract/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using SMDH.Models;
+using SMDH.Models.ViewModels;
+
+namespace SMDH.Models.Abstract
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+        IQueryable<Item> Items { get; }
+        IQueryable<Customer> Customers { get; }
+
+        Product Find(int productId);
+
+        bool Remove(int productId);
+        bool Create(Product product);
+        bool Edit(Product product);
+    }
+}
---
+++ trunk/Wip/DEV_Source/SMDH/SMDH/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMDH.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        public int ProductCategory { get; set; }
+        public string Name { get; set; }
+        public string ProductWidth { get; set; }
+        public string ProductHeight { get; set; }
+        public string ProductLength { get; set; }
+        public string ProductWeight { get; set; }
+        public string ProductPrice { get; set; }
+        public int CustomerId { get; set; }
+        public bool IsPermanent { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
